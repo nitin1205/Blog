9c4051935e888bdd25c367bb8c611b9a79cc1c77
nitin1205 <vortechcorp@gmail.com>
2022-07-05 23:46:44 +0530

    made basic structure

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const articleRouter = require('./routes/articles');
+const Article = require('./models/article');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true });
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/articles', articleRouter);
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' });
+    res.render('articles/index', {articles: articles});
+})
+
+app.listen(5000)
